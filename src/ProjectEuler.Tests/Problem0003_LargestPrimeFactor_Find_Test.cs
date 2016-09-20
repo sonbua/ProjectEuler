fe1a74df2dab84eb67080d7d79c47e91a128c43b
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-09-21 00:32:53 +0700

    Solve problem 3: Largest prime factor.

---
+++ src/ProjectEuler.Tests/Problem0003_LargestPrimeFactor_Find_Test.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class LargestPrimeFactor_Find_Test
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 2)]
+        [InlineData(5, 5)]
+        [InlineData(6, 3)]
+        [InlineData(8, 2)]
+        [InlineData(10, 5)]
+        [InlineData(13195, 29)]
+        public void GivenANumber_ReturnsCorrectLargestPrimeFactor(long number, long expectedLargestPrimeFactor)
+        {
+            // arrange
+            var sut = new LargestPrimeFactor();
+
+            // act
+            var largestPrimeFactor = sut.Find(number);
+
+            // assert
+            Assert.Equal(expectedLargestPrimeFactor, largestPrimeFactor);
+        }
+    }
+}
---
+++ src/ProjectEuler/Problem0003_LargestPrimeFactor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    /// <summary>
+    /// Problem 3: Largest prime factor
+    /// https://projecteuler.net/problem=3
+    /// 
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// </summary>
+    public class LargestPrimeFactor
+    {
+        public long Find(long number)
+        {
+            return YieldFactors(number).Last();
+        }
+
+        private IEnumerable<long> YieldFactors(long number)
+        {
+            if (number <= 3)
+            {
+                yield return number;
+                yield break;
+            }
+
+            for (var factor = 2; factor <= Math.Sqrt(number);)
+            {
+                if (number%factor == 0)
+                {
+                    yield return factor;
+                    number /= factor;
+                    continue;
+                }
+                factor++;
+            }
+
+            yield return number;
+        }
+    }
+}
