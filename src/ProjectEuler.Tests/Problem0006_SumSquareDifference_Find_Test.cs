f108e574eef800a554a9a2d6033bee47e15e1b1b
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-09-28 00:12:00 +0700

    Solve problem 6: Sum square difference.

---
+++ src/ProjectEuler.Tests/Problem0006_SumSquareDifference_Find_Test.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class SumSquareDifference_Find_Test
+    {
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(2, 4)]
+        [InlineData(3, 22)]
+        [InlineData(10, 2640)]
+        public void GivenAnInclusiveUpper_ReturnsSumSquareDifference(int inclusiveUpperNumber, int expectedSumSquareDifference)
+        {
+            // arrange
+            var sut = new SumSquareDifference();
+
+            // act
+            var sumSquareDifference = sut.Find(inclusiveUpperNumber);
+
+            // assert
+            Assert.Equal(expectedSumSquareDifference, sumSquareDifference);
+        }
+    }
+}
---
+++ src/ProjectEuler/Problem0006_SumSquareDifference.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace ProjectEuler
+{
+    /// <summary>
+    /// Problem 6: Sum square difference
+    /// https://projecteuler.net/problem=6
+    /// 
+    /// The sum of the squares of the first ten natural numbers is,
+    /// 12 + 22 + ... + 102 = 385
+    ///  The square of the sum of the first ten natural numbers is,
+    /// (1 + 2 + ... + 10)2 = 552 = 3025
+    ///  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+    ///  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    /// </summary>
+    public class SumSquareDifference
+    {
+        public int Find(int inclusiveUpperNumber)
+        {
+            if (inclusiveUpperNumber == 1)
+                return 0;
+
+            return Enumerable.Range(1, inclusiveUpperNumber - 1)
+                             .SelectMany(i => Enumerable.Range(i + 1, inclusiveUpperNumber - i),
+                                         (i, j) => i * j)
+                             .Aggregate((seed, i) => seed + i) * 2;
+        }
+    }
+}
