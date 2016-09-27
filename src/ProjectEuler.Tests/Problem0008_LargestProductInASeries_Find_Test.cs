15ef56efa6e402b90223d1f94b2a33c07564aebd
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-09-28 00:23:31 +0700

    Solve problem 8: Largest product in a series.

---
+++ src/ProjectEuler.Tests/Problem0008_LargestProductInASeries_Find_Test.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class LargestProductInASeries_Find_Test
+    {
+        [Theory]
+        [InlineData("12", 1, 2)]
+        [InlineData("123", 1, 3)]
+        [InlineData("123", 2, 6)]
+        [InlineData("82166370484403199890008895243450658541227588666881", 4, 5832)]
+        public void GivenANumberString_ReturnsLargestProductInASeries(string series, int sequenceLength, long expectedLargestProduct)
+        {
+            // arrange
+            var sut = new LargestProductInASeries();
+
+            // act
+            var largestProduct = sut.Find(series, sequenceLength);
+
+            // assert
+            Assert.Equal(expectedLargestProduct, largestProduct);
+        }
+    }
+}
