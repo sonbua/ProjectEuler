0aa72af9d992559ee562b99616e891fc07ea3912
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-09-28 00:17:50 +0700

    Solve problem 7: 10001st prime.

---
+++ src/ProjectEuler.Tests/Problem0007_Prime_Find_Test.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class Prime_Find_Test
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        [InlineData(3, 5)]
+        [InlineData(4, 7)]
+        [InlineData(5, 11)]
+        [InlineData(6, 13)]
+        public void GivenANumber_ReturnsPrimeOfThatOrder(int order, long expectedPrime)
+        {
+            // arrange
+            var sut = new Prime();
+
+            // act
+            var prime = sut.Find(order);
+
+            // assert
+            Assert.Equal(expectedPrime, prime);
+        }
+    }
+}
---
+++ src/ProjectEuler/Problem0007_Prime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    /// <summary>
+    /// Problem 7: 10001st prime
+    /// https://projecteuler.net/problem=7
+    /// 
+    /// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    /// What is the 10 001st prime number?
+    /// </summary>
+    public class Prime
+    {
+        private static readonly IList<long> _primes = new List<long> {2};
+
+        private static long NextPrime
+        {
+            get
+            {
+                var number = _primes.Last() + 1;
+
+                while (!IsPrime(number))
+                    number++;
+
+                return number;
+            }
+        }
+
+        public long Find(int order)
+        {
+            if (order <= _primes.Count)
+                return _primes.ElementAt(order - 1);
+
+            while (_primes.Count < order)
+                _primes.Add(NextPrime);
+
+            return _primes.Last();
+        }
+
+        private static bool IsPrime(long number)
+        {
+            for (var i = 2L; i <= Math.Sqrt(number); i++)
+                if (number%i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
