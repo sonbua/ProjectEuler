f9be97d044c8d2ccf49c4c9e08bde0f3cc4f1410
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-09-28 00:28:42 +0700

    Solve problem 9: Special Pythagorean triplet.

---
+++ src/ProjectEuler.Tests/Problem0009_SpecialPythagoreanTriplet_FindProduct_Test.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class SpecialPythagoreanTriplet_FindProduct_Test
+    {
+        [Theory]
+        [InlineData(12, 60)]
+        [InlineData(30, 780)]
+        public void GivenSumOfPythagoreanTriplet_ReturnsProduct(int tripletSum, int expectedProduct)
+        {
+            // arrange
+            var sut = new SpecialPythagoreanTriplet();
+
+            // act
+            var product = sut.FindProduct(tripletSum);
+
+            // assert
+            Assert.Equal(expectedProduct, product);
+        }
+    }
+}
---
+++ src/ProjectEuler/Problem0009_SpecialPythagoreanTriplet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    /// <summary>
+    /// Problem 9: Special Pythagorean triplet
+    /// https://projecteuler.net/problem=9
+    /// 
+    /// A Pythagorean triplet is a set of three natural numbers, a &lt; b &lt; c, for which,
+    /// a2 + b2 = c2
+    /// For example, 32 + 42 = 9 + 16 = 25 = 52.
+    /// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    /// Find the product abc.
+    /// </summary>
+    public class SpecialPythagoreanTriplet
+    {
+        public int FindProduct(int tripletSum)
+        {
+            for (var i = 1; i < tripletSum - 2; i++)
+                for (var j = 1; j < tripletSum - i - 1; j++)
+                {
+                    var k = tripletSum - i - j;
+
+                    if (i*i + j*j == k*k)
+                        return i*j*k;
+                }
+            throw new NotSupportedException();
+        }
+    }
+}
