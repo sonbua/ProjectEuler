3e10cf676e5e244ffc88ce4df2d467a2383c99c1
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-09-29 20:09:29 +0700

    Solve problem 12: Highly divisible triangular number.

---
+++ src/ProjectEuler.Tests/Problem0012_HighlyDivisibleTriangularNumber_FindDivisors_Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class HighlyDivisibleTriangularNumber_FindDivisors_Test
+    {
+        [Theory]
+        [InlineData(1, new long[] {1})]
+        [InlineData(3, new long[] {1, 3})]
+        [InlineData(6, new long[] {1, 2, 3, 6})]
+        public void GivenANumber_ReturnsTheDivisors(long number, long[] expectedDivisors)
+        {
+            // arrange
+            var sut = new HighlyDivisibleTriangularNumber();
+
+            // act
+            var divisors = sut.FindDivisors(number).OrderBy(x => x);
+
+            // assert
+            Assert.True(expectedDivisors.OrderBy(x => x).SequenceEqual(divisors));
+        }
+    }
+}
---
+++ src/ProjectEuler.Tests/Problem0012_HighlyDivisibleTriangularNumber_Find_Test.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class HighlyDivisibleTriangularNumber_Find_Test
+    {
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(3, 6)]
+        [InlineData(5, 28)]
+        public void GivenANumberOfDivisors_ReturnsTheSmallestTriangleNumber(int numberOfDivisors, long expectedTriangleNumber)
+        {
+            // arrange
+            var sut = new HighlyDivisibleTriangularNumber();
+
+            // act
+            var triangleNumber = sut.Find(numberOfDivisors);
+
+            // assert
+            Assert.Equal(expectedTriangleNumber, triangleNumber);
+        }
+    }
+}
