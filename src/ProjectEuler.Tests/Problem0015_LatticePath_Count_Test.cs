4567f73141646697d2c6be21fc09d1c9edce5c22
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-11-06 01:14:34 +0700

    Clean up.

--- src/ProjectEuler.Tests/Problem0015_LatticePath_Count_Test.cs
+++ src/ProjectEuler.Tests/Problem0015_LatticePath_Count_Test.cs
@@ -5,10 +5,9 @@ namespace ProjectEuler.Tests
     public class LatticePath_Count_Test
     {
         [Theory]
-//        [InlineData(1, 2)]
-//        [InlineData(2, 6)]
+        [InlineData(1, 2)]
+        [InlineData(2, 6)]
         [InlineData(3, 20)]
-//        [InlineData(20, 137846528820)]
         public void GivenAGridSize_ReturnsCorrectPathCount(int gridSize, long expectedPathCount)
         {
             // arrange
