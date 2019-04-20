e24c84b07fa9c121f80e6235397c98501f844c44
Son Nguyen <dinh.son.nguyen@gmail.com>
2019-04-20 13:09:57 +0700

    Reformat problem's description in XML documentation.

--- src/ProjectEuler/Problem0006_SumSquareDifference.cs
+++ src/ProjectEuler/Problem0006_SumSquareDifference.cs
@@ -7,11 +7,11 @@ namespace ProjectEuler
     /// https://projecteuler.net/problem=6
     /// 
     /// The sum of the squares of the first ten natural numbers is,
-    /// 12 + 22 + ... + 102 = 385
-    ///  The square of the sum of the first ten natural numbers is,
-    /// (1 + 2 + ... + 10)2 = 552 = 3025
-    ///  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-    ///  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    /// 1^2 + 2^2 + ... + 10^2 = 385
+    /// The square of the sum of the first ten natural numbers is,
+    /// (1 + 2 + ... + 10)^2 = 55^2 = 3025
+    /// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+    /// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
     /// </summary>
     public class SumSquareDifference
     {
